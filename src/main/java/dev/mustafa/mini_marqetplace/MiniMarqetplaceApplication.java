9724590a5a314eb9215e66c0fb71499dcf4ffa0c
Jaxongirshoh <jakhongirshohkhudoyorov@gmail.com>
2026-09-09 13:41:35 +0500

    initial comment

---
+++ src/main/java/dev/mustafa/mini_marqetplace/MiniMarqetplaceApplication.java
@@ -0,0 +1,13 @@
+package dev.mustafa.mini_marqetplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMarqetplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMarqetplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mustafa/mini_marqetplace/MiniMarqetplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mustafa.mini_marqetplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMarqetplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
